3fb730f80799a4373d2c8c1ce6a07b588fbc839e
Lisha Kuinkel <ltkuinkel@gmail.com>
2023-09-18 21:29:34 -0600

    re-adding home to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,6 +16,7 @@ const Navbar = ({handlePageChange}) => {
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-5 text-[##f5f5dc]'>
         <h1 className='w-full text-3xl font-bold'></h1>
         <ul className='hidden md:flex  text-[#819ebb]'>
+          <li className='p-4  hover:text-[#cfd9e3]'><a href="/" onClick={()=> handlePageChange('Home')}>Home</a></li>
           <li className='p-4  hover:text-[#cfd9e3]'><a href="#about" onClick={()=> handlePageChange('AboutMe')}>About</a></li>
           <li className='p-4  hover:text-[#cfd9e3]'><a href="#portfolio" onClick={()=> handlePageChange('Portfolio')}>Portfolio</a></li>
           <li className='p-4  hover:text-[#cfd9e3]'><a href="#contact" onClick={()=> handlePageChange('ContactMe')}>Contact</a></li>
@@ -32,6 +33,7 @@ const Navbar = ({handlePageChange}) => {
       <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-white-900 bg-white m-4 ease-in-out duration-500' : 'fixed left-[-100%]'}>
       
         <ul className='pt-14 uppercase'>
+        
           <li className='p-4 border-b border-white-600'><a href="#about" onClick={()=> handlePageChange('AboutMe')}>About</a></li>
           <li className='p-4 border-b border-white-600' ><a href="#portfolio" onClick={()=> handlePageChange('Portfolio')}>Portfolio</a></li>
           <li className='p-4 border-b border-white-600'><a href="#contact" onClick={()=> handlePageChange('ContactMe')}>Contact</a></li>
