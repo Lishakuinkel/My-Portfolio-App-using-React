de9d8fcb415d28deeebc028fbfa4430986fd9a7b
Lisha Kuinkel <ltkuinkel@gmail.com>
2023-09-18 21:10:46 -0600

    removed home from navbar to avoid bugs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from '../src/components/Layout'
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
